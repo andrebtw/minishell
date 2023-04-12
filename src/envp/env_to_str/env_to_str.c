25b89d59965ea6779e4d1edee8b8b7016f2f0d70
Maxime Thibault <mthibaul@z2r9p6.42lyon.fr>
2023-04-12 13:01:58 +0200

    Added conversion of envp from limked list to char **

--- src/envp/env_to_str/env_to_str.c
+++ src/envp/env_to_str/env_to_str.c
@@ -11,19 +11,30 @@
 /* ************************************************************************** */
 #include "minishell.h"
 
-int env_size(t_env *env);
+int		env_size(t_env *env);
+int		add_env(t_env *env, char *env_str);
+void	free_env_str(char **env_str);
 
 char **env_to_str(t_env *env)
 {
-	char **env_str;
+	char	**env_str;
+	int		i;
 
 	env_str = malloc(sizeof(char *) * env_size(env));
 	if (!env_str)
 		return (NULL);
-
+	i = 0;
+	while (env)
+	{
+		if (add_env(env,  env_str[i]) > 0)
+			return (free_env_str(env_str), NULL);
+		env++;
+		i++;
+	}
+	return (env_str);
 }
 
-int env_size(t_env *env)
+int	env_size(t_env *env)
 {
 	int i;
 
@@ -34,4 +45,30 @@ int env_size(t_env *env)
 		i++;
 	}
 	return (i);
+}
+
+int	add_env(t_env *env, char *env_str)
+{
+	int len;
+
+	len = (int) ft_strlen(env->name) + (int) ft_strlen(env->value) + 2;
+	env_str = malloc(sizeof(char) * len);
+	if (!env_str)
+		return (1);
+	ft_strcat(env_str, env->name);
+	ft_strcat(env_str, "=");
+	ft_strcat(env_str, env->value);
+}
+
+void	free_env_str(char **env)
+{
+	int i;
+
+	i = 0;
+	while (env[i])
+	{
+		free(env[i]);
+		i++;
+	}
+	free(env);
 }
