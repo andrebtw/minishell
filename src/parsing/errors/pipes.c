3af2a674bbb21da3885db82cf61653620a5b3b1b
andrebtw <43089870+andrebtw@users.noreply.github.com>
2023-05-09 20:01:43 +0000

    Pipe errors finished.

--- src/parsing/errors/pipes.c
+++ src/parsing/errors/pipes.c
@@ -6,11 +6,23 @@
 /*   By: anrodri2 <anrodri2@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 13:02:54 by anrodri2          #+#    #+#             */
-/*   Updated: 2023/05/08 12:15:14 by anrodri2         ###   ########.fr       */
+/*   Updated: 2023/05/09 20:00:50 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../incl/minishell.h"
+
+int	check_if_more_pipes(t_shell *shell, size_t i)
+{
+	while (shell->input[i] && ft_strchr(" \t", shell->input[i]))
+		i++;
+	if (!shell->input[i])
+		return (ft_putstr_fd(PIPE_ERR_MSG, 2), TRUE);
+	if (shell->input[i] == '|')
+		return (ft_putstr_fd(PIPE_ERR_MSG2, 2), TRUE);
+	ft_putstr_fd(PIPE_ERR_MSG, 2);
+	return (TRUE);
+}
 
 int	pipe_loop(t_shell *shell, size_t i, int state)
 {
@@ -19,22 +31,15 @@ int	pipe_loop(t_shell *shell, size_t i, int state)
 		state = quotes_state_error(shell, i, state);
 		if (state == NOT_INIT)
 		{
-			while (ft_strchr(" \t", shell->input[i]))
-				i++;
-			if (shell->input[i] == '|')
+			if (ft_strchr("|", shell->input[i]))
 			{
-				while (ft_strchr(" \t", shell->input[i]))
+				i++;
+				while (shell->input[i] && ft_strchr(" \t", shell->input[i]))
 					i++;
 				if (shell->input[i] == '|')
-				{
-					while (ft_strchr(" \t", shell->input[i]))
-						i++;
-					if (shell->input[i])
-						i++;
-					if (shell->input[i] == '|')
-						return (ft_putstr_fd(PIPE_ERR_MSG2, 2), TRUE);
+					return (check_if_more_pipes(shell, i));
+				if (!shell->input[i])
 					return (ft_putstr_fd(PIPE_ERR_MSG, 2), TRUE);
-				}
 			}
 		}
 		i++;
@@ -61,7 +66,7 @@ int	pipe_check(t_shell *shell, size_t i, int state)
 		return (ft_putstr_fd(PIPE_ERR_MSG, 2), TRUE);
 	}
 	i = 0;
-	// if (pipe_loop(shell, i, state))
-	// 	return (TRUE);
+	if (pipe_loop(shell, i, state))
+		return (TRUE);
 	return (FALSE);
 }
