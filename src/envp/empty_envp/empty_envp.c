a3363384b9d1c5fc390e6e6eab8e414efa1b76fa
André <43089870+andrebtw@users.noreply.github.com>
2023-07-24 14:48:36 +0000

    FIX: Exit works with no env.

--- src/envp/empty_envp/empty_envp.c
+++ src/envp/empty_envp/empty_envp.c
@@ -6,7 +6,7 @@
 /*   By: anrodri2 <anrodri2@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 15:18:36 by anrodri2          #+#    #+#             */
-/*   Updated: 2023/06/13 09:27:17 by anrodri2         ###   ########.fr       */
+/*   Updated: 2023/07/24 14:47:12 by anrodri2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ t_env	*empty_envp(void)
 	pwd = get_pwd();
 	if (!pwd)
 		return (NULL);
-	new_env = env_create("PWD", pwd, TRUE);
-	if (envadd_elem(new_env, "SHLVL", "1", TRUE))
+	new_env = env_create(ft_strdup("PWD"), pwd, TRUE);
+	if (envadd_elem(new_env, ft_strdup("SHLVL"), ft_strdup("1"), TRUE))
 		return (NULL);
 	if (!new_env)
 		return (NULL);
